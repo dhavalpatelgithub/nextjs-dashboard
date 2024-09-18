5a661d126b1c0486f2e2c8e5c53fad6e8bb735f0
Dhaval Patel <pateldhaval891@gmail.com>
2024-09-18 16:31:32 +0530

    redirect to login

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,8 +3,12 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signout } from '@/app/lib/actions';
+import { useRouter } from 'next/navigation';
 
 export default function SideNav() {
+
+  const router = useRouter();
+  
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -18,11 +22,10 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        {/* <form 
-            action={signOutUser}> */}
         <form
           action={async () => {
             await signout();
+            router.push('/login');
           }}>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
